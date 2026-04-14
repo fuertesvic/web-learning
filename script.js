3ab8970e0aa1d73548e101c02bfe90f84e4a1f5e
fuertesvic <fuertesvic@gmail.com>
2026-04-14 16:22:26 +0200

    Es crea script.js amb una primera interacció sobre un botó

---
+++ script.js
@@ -0,0 +1,16 @@
+let button = document.getElementById("helloButton");
+let message = document.getElementById("messageText")
+
+button.addEventListener("click", function() {
+
+    button.textContent = "Clicat!";
+
+    if (message.textContent == "Aquest text canviarà..."){
+        message.textContent = "El text ha canviat!";
+    }
+    else{
+        message.textContent = "Aquest text canviarà...";
+    }
+    
+
+});
