b92388c6cbeb7d1769f681b7cdaeecc93442c089
fuertesvic <fuertesvic@gmail.com>
2026-04-28 17:02:52 +0200

    Tasks: Refactor, ara és data-driven i no ha de llegir l'html cada vegada

--- tasks.js
+++ tasks.js
@@ -1,89 +1,177 @@
-    // Organitzador de Tasques
-    /* 
+// Organitzador de Tasques
+/* 
 
-    Autor: Víctor Fuertes Centeno
-    Data : Abril de 2026
-    Descripció: Classe que encapsula la lògica de creació, esborrar i guardar en la memòria del navegador les tasques.
-    --Aquesta classe forma part del projecte web_learn, on estic auto-aprenent desenvolupament web--
-    */
+Autor: Víctor Fuertes Centeno
+Data : Abril de 2026
+Descripció: Classe que encapsula la lògica de creació, esborrar i guardar en la memòria del navegador les tasques.
+--Aquesta classe forma part del projecte web_learn, on estic auto-aprenent desenvolupament web--
+*/
+
+// Array de tasques
+let tasks = [];
+
+// Filtre seleccionat, per controlar quines tasques es mostren. Pot ser "all", "pending" o "completed".
+let currentFilter = "all";
+
+// Inicialitzar els elements HTML: Inputs, botons, comptadors, etc.
+let taskInput = document.getElementById("taskInput");
+let addTaskButton = document.getElementById("addTaskButton");
+let taskList = document.getElementById("taskList");
+let showAllTasksButton = document.getElementById("showAllTasksButton");
+let showPendingTasksButton = document.getElementById("showPendingTasksButton");
+let showCompletedTasksButton = document.getElementById("showCompletedTasksButton");
+let allTasksCounter = document.getElementById("allTaskCount");
+let pendingTasksCounter = document.getElementById("pendingTaskCount"); 
+let completedTasksCounter = document.getElementById("completedTaskCount"); 
+
+
+// Lògica de l'input per Afegir Tasca
+taskInput.addEventListener("keydown", function(event) {
+
+    if (event.key === "Enter") {
+        
+    // Agafar l'input de l'usuari, validar que no és nul
+    let taskText = taskInput.value;
+
+    if (taskText === "") {
+        return;
+    }
+
+    // Crea la tasca
+    createTaskItem(taskText, taskText.completed); 
     
+    // Neteja el contingut de l'input
+    taskInput.value = "";
 
-    // Inicialitzar els elements de l'html: Inputs, botons, comptadors, etc.
-    let taskInput = document.getElementById("taskInput");
-    let addTaskButton = document.getElementById("addTaskButton");
-    let taskList = document.getElementById("taskList");
-    let showAllTasksButton = document.getElementById("showAllTasksButton");
-    let showPendingTasksButton = document.getElementById("showPendingTasksButton");
-    let showCompletedTasksButton = document.getElementById("showCompletedTasksButton");
-    let allTasksCounter = document.getElementById("allTaskCount");
-    let pendingTasksCounter = document.getElementById("pendingTaskCount"); 
-    let completedTasksCounter = document.getElementById("completedTaskCount"); 
+    saveTasks();
+    }
+});
+
+// Event de Click al botó d'afegir tasca
+addTaskButton.addEventListener("click", function(){
+
+    // Agafar l'input de l'usuari, validar que no és nul
+    let taskText = taskInput.value;
+
+    if (taskText === "") {
+
+        return;
+    }
+
+    // Crea la tasca
+    createTaskItem(taskText, taskText.completed); 
+
+    // Neteja el contingut de l'input
+    taskInput.value = "";
+    saveTasks();
+});
+
+
+// funció per crear tasques, 
+// utilitzada pel botó (Afegir Tasca) i també per l'inicialització de tasques guardades a la memòria.
+function createTaskItem(taskText, isCompleted = false) {
+
+    tasks.push({
+        text: taskText,
+        completed: isCompleted
+    })
+    renderTasks();
+}
+
+
+// Llògica pel botó "Totes", de mostrar totes les tasques
+showAllTasksButton.addEventListener("click", function() {
+    currentFilter = "all";
+    renderTasks();
+});
+
+
+// Llògica pel botó "Per Completar", de mostrar totes les tasques
+showPendingTasksButton.addEventListener("click", function() {
+    currentFilter = "pending";
+    renderTasks();
+});
+
+
+// Llògica pel botó "Completades"
+showCompletedTasksButton.addEventListener("click", function() {
+    currentFilter = "completed";
+    renderTasks();
+});
+
+
+// Funció per actuallitzar els comptadors de tasques
+function updateTaskCounts() {
+
+    let pending = 0; 
+    let completed = 0;
     
+    tasks.forEach(function(task){
 
-    // Lògica de l'input per Afegir Tasca
-    taskInput.addEventListener("keydown", function(event) {
-
-        if (event.key === "Enter") {
-            // Agafar l'input de l'usuari, validar que no és nul
-        let taskText = taskInput.value;
-
-        if (taskText === "") {
-
-            return;
+        if (task.completed){
+            completed++;
         }
 
-        // Crea la tasca
-        createTaskItem(taskText, taskText.completed); 
-
-        // Neteja el contingut de l'input
-        taskInput.value = "";
-
-        // Afegir el nou LI a la llista (UL) anomenada taskList i guarda la tasca
-        saveTasks();
-        }
+        else {
+            pending++;
+        };
     });
 
-    // Event de Click al botó d'afegir tasca
-    addTaskButton.addEventListener("click", function(){
+    completedTasksCounter.textContent = completed;
+    pendingTasksCounter.textContent = pending;
+    allTasksCounter.textContent = tasks.length;
+}
 
-        // Agafar l'input de l'usuari, validar que no és nul
-        let taskText = taskInput.value;
 
-        if (taskText === "") {
+// funció per guardar tasques encara que es refresqui el navegador, guarda totes les tasques actuals
+function saveTasks() {
 
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+
+}
+
+// funció per carregar tasques des de la memòria del navegador
+function loadTasks(){
+    
+    let savedTasks = localStorage.getItem("tasks");
+
+    tasks = JSON.parse(savedTasks)
+
+    renderTasks();
+}
+
+function renderTasks(){
+
+    console.log("Rendering Tasks!");
+
+    // Esborra el contingut html
+    taskList.innerHTML = "";
+
+    // Itera l'array de tasques i manté un index
+    tasks.forEach(function(task, index) {
+
+        if (currentFilter === "pending" && task.completed) {
             return;
         }
-
-        // Crea la tasca
-        createTaskItem(taskText, taskText.completed); 
-
-        // Neteja el contingut de l'input
-        taskInput.value = "";
-
-        // Afegir el nou LI a la llista (UL) anomenada taskList i guarda la tasca
-        saveTasks();
-        updateTaskCounts();
-    });
-
-
-    // funció per crear tasques, 
-    // utilitzada pel botó (Afegir Tasca) i també per l'inicialització de tasques guardades a la memòria.
-    function createTaskItem(taskText, isCompleted = false) {
-
-        // Crea la tasca i assigna el text de l'Input
+        if (currentFilter === "completed" && !task.completed){
+            return;
+        }
         let newTask = document.createElement("li");
-
-        newTask.textContent = taskText;
-
-        if (isCompleted){
+        newTask.textContent = task.text ;
+        if (task.completed) {
             newTask.classList.add("completed");
         }
 
         // Possibilitat de completar tasques fent-li clic.
         newTask.addEventListener("click", function(){
-            newTask.classList.toggle("completed");
+            
+            // Això fa canviar d'estat de false a true o true a false. "Interruptor".
+            tasks[index].completed = !tasks[index].completed;
+            
+            // Cal guardar renderitzar de nou
             saveTasks();
             updateTaskCounts();
+            renderTasks();
         });
 
         // Botó d'esborrar tasca
@@ -91,161 +179,24 @@
         removeButton.textContent = "❌";
 
         removeButton.addEventListener("click", function(event){
+            console.log(tasks[index]);
             event.stopPropagation();
-            newTask.remove();
+
+            // Elimina l'element de l'array seleccionat.
+            tasks.splice(index, 1);
             saveTasks();
             updateTaskCounts();
+            renderTasks();
         
         });
 
-        // Possibilitat d'Editar Tasca
-        newTask.addEventListener("dblclick", function(){
-        let currentText = newTask.firstChild.textContent;
-        let input = document.createElement("input");
-        input.type = "text";
-        input.value = currentText;
-        newTask.firstChild.replaceWith(input);
-        input.focus();
-
-        input.addEventListener("keydown", function(event) {
-
-            if (event.key === "Enter") {
-
-                let updatedText = input.value;
-
-                let textNode = document.createTextNode(updatedText);
-
-                input.replaceWith(textNode);
-
-                saveTasks();
-                updateTaskCounts();
-
-            }
-
-        });
-
-        });
-
-        // Afegir botó d'esborrar a la tasca, i la tasca a la llista de tasques.
+        // Afegeix el botó a la tasca, i la tasca a la llista.
         newTask.appendChild(removeButton);
         taskList.appendChild(newTask);
 
-        updateTaskCounts();
-
-    }
-
-
-    // Llògica pel botó "Totes", de mostrar totes les tasques
-    showAllTasksButton.addEventListener("click", function() {
-        // Selecciona totes les tasques
-        let taskItems = document.querySelectorAll("#taskList li");
-        // Recorre totes les tasques
-        taskItems.forEach(function(task){
-            
-            // Mostra la tasca
-            task.style.display = "";
-
-        });
     });
+    updateTaskCounts();
+}
 
 
-    // Llògica pel botó "Per Completar", de mostrar totes les tasques
-    showPendingTasksButton.addEventListener("click", function() {
-        let taskItems = document.querySelectorAll("#taskList li");
-        taskItems.forEach(function(task){
-            if (task.classList.contains("completed")){
-                // Amaga la tasca
-                task.style.display = "none";
-            }
-            else{
-                task.style.display = "";
-            }
-
-        });
-    });
-
-
-    // Llògica pel botó "Completades"
-    showCompletedTasksButton.addEventListener("click", function() {
-        let taskItems = document.querySelectorAll("#taskList li");
-        taskItems.forEach(function(task){
-        
-            if (task.classList.contains("completed")){
-
-                task.style.display = "";
-            }
-            else{
-                task.style.display = "none";
-            }
-
-        });
-
-    });
-
-
-    // Funció per actuallitzar els comptadors de tasques
-    function updateTaskCounts() {
-
-        let taskItems = document.querySelectorAll("#taskList li");
-        let pending = 0; 
-        let completed = 0;
-        
-        taskItems.forEach(function(task){
-
-            if (task.classList.contains("completed")){
-                completed++;
-            }
-
-            else {
-                pending++;
-            };
-        });
-
-        completedTasksCounter.textContent = completed;
-        pendingTasksCounter.textContent = pending;
-        allTasksCounter.textContent = taskItems.length;
-    }
-
-
-    // funció per guardar tasques encara que es refresqui el navegador, guarda totes les tasques actuals
-    function saveTasks() {
-
-        let tasks = [];
-
-        let taskItems = document.querySelectorAll("#taskList li");
-
-        taskItems.forEach(function(task) {
-
-            let taskObject = {
-                    
-                text: task.firstChild.textContent,
-                completed: task.classList.contains("completed")
-
-            };
-
-            tasks.push(taskObject);
-
-        }); 
-
-        localStorage.setItem("tasks", JSON.stringify(tasks));
-
-    }
-
-    // funció per carregar tasques des de la memòria del navegador
-    function loadTasks(){
-        let savedTasks = localStorage.getItem("tasks");
-
-        if (savedTasks === null) {
-            return;
-        }
-
-        let tasks = JSON.parse(savedTasks);
-
-        tasks.forEach(function(task){
-            createTaskItem(task.text, task.completed);
-        });
-
-    }
-
-
-    loadTasks();
+loadTasks();
